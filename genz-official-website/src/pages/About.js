7bd0c6a90b7758645eaa93e60ced82b33436528f
ChathumiFernando-code <126304830+ChathumiFernando-code@users.noreply.github.com>
2023-04-17 21:12:41 +0530

    About Us
    About-us page completed

--- genz-official-website/src/pages/About.js
+++ genz-official-website/src/pages/About.js
@@ -1,9 +1,13 @@
 import React from "react";
 import AboutLandingSection from "../components/AboutLandingSection";
+import AboutLeadSection from "../components/AboutLeadSection";
+import AboutFamilySection from "../components/AboutFamilySection";
 export const About = () => {
   return (
     <>
       <AboutLandingSection />
+      <AboutLeadSection />
+      <AboutFamilySection />
     </>
   );
 };
